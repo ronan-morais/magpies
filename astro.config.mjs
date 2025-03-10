ab7d297e5e49b126582121e512f33a96daa6cff9
Ronan Alves de Morais <ronan86@gmail.com>
2025-03-10 15:11:30 +1030

    images

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
